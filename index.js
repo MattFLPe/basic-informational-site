2ae94044e781ed6c49286f40277a39e63f6b3cc6
Mateus Felipe <brasil.mateus748@gmail.com>
2024-10-29 23:50:49 -0300

    Added path check to check if URL is root

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const path = require('path');
 
 http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    const filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
+    let filePath = req.url === '/' ? '/index.html' : req.url;
+    filePath = path.join(__dirname, filePath);
+    fs.readFile(filePath, function(err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
             return res.end('404 not found')
